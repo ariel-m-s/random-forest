5086381fc3b264a2ebad87f1a53d7950bb92987d
Ariel Martínez <amartinez5@uc.cl>
2019-10-26 00:27:32 -0300

    :sparkles: add random sample from dataframe
    selects randomly rows and cols

--- preprocess.py
+++ preprocess.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 def load_dfs(path, frac=0.8):
     """
-    path: `str`
+    :param path: CSV file path containing the data
+    :type path: `str`
 
-    return: `tuple[DataFrame]`
+    :returns: train and test datasets
+    :rtype: `tuple[DataFrame]`
     """
     df = pd.read_csv(path).dropna()
     train = df.sample(frac=frac)
@@ -13,7 +15,25 @@ def load_dfs(path, frac=0.8):
     return train, test
 
 
+def random_sample(df, target, shape):
+    """
+    :param df: dataset to choose random samples from
+    :type df: `DataFrame`
+    :param target: series of df with the class to predict
+    :type target: `Series`
+
+    :returns: random subdataset + target dataset
+    :rtype: `DataFrame`
+    """
+    n_rows, n_cols = shape
+    df = df.sample(n=n_cols, axis=1)
+    df[target.name] = target
+    df = df.sample(n=n_rows)
+    return df
+
+
 if __name__ == "__main__":
     train, test = load_dfs("data.csv")
-    print(train.head())
-    print(test.head())
+    # print(random_samples(train, 5, 5))
+    print(random_sample(train.drop("Class", axis=1), train["Class"], (1, 1)))
+    print(train["Class"])
