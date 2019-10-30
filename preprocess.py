b073914bc77c1f435118a9018ef518603086f96f
Ariel Martínez Silberstein <amartinez5@uc.cl>
2019-10-30 15:03:09 -0300

    Data preprocessing (#1)
    * :heavy_plus_sign: add flake8 dev dependency
    
    * :see_no_evil: add data to gitignore
    
    * :sparkles: add data loading
    
    this includes reading CSV, cleansing and splitting into sets
    
    * :sparkles: add random sample from dataframe
    
    selects randomly rows and cols
    
    * :see_no_evil: add manuals to gitignore
    
    * :bulb: update docstring
    
    * :pencil2: fix docstring typo

---
+++ preprocess.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+def load_dfs(path, frac=0.8):
+    """
+    :param path: CSV file path containing the data
+    :type path: `str`
+
+    :returns: train and test datasets
+    :rtype: `tuple[pandas.DataFrame]`
+    """
+    df = pd.read_csv(path).dropna()
+    train = df.sample(frac=frac)
+    test = df.drop(train.index)
+    return train, test
+
+
+def random_sample(df, target, shape):
+    """
+    :param df: dataset to choose a random sample from
+    :type df: `pandas.DataFrame`
+    :param shape: dimensions of the sample
+    :type shape: `tuple[int]`
+    :param target: series of df with the class to predict
+    :type target: `pandas.Series`
+
+    :returns: random subdataset + target dataset
+    :rtype: `pandas.DataFrame`
+    """
+    n_rows, n_cols = shape
+    df = df.sample(n=n_cols, axis=1)
+    df[target.name] = target
+    df = df.sample(n=n_rows)
+    return df
+
+
+if __name__ == "__main__":
+    train, test = load_dfs("data.csv")
+    print(random_sample(train.drop("Class", axis=1), train["Class"], (1, 1)))
+    print(train["Class"])
